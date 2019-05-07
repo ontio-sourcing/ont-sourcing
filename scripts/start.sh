1c36d0f5abdb7d9eff76111b6f7f99ef7a0a23f8
leewi9MBP <2768881880@qq.com>
2019-05-07 12:22:38 +0800

    stability improvement;

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #
 APPNAME="ont-sourcing-0.0.1-SNAPSHOT.jar"
 APPLOCATION="/home/ubuntu/ont-sourcing/target/ont-sourcing-0.0.1-SNAPSHOT.jar"
-APPENV="remotetest"
+APPENV="/home/ubuntu/ont-sourcing/config/application-remotetest.properties"
 APPPORT=7088
 LOGLOCATION="/home/ubuntu/ont-sourcing/log/all.log"
 
@@ -11,8 +11,11 @@ LOGLOCATION="/home/ubuntu/ont-sourcing/log/all.log"
 ps -ef | grep $APPNAME | grep -v grep | awk '{print $2}' | xargs kill -9
 
 #
-echo "nohup java -Dspring.profiles.active=$APPENV -Dserver.port=$APPPORT -jar $APPLOCATION >/dev/null 2>&1 &"
-nohup java -Dspring.profiles.active=$APPENV -Dserver.port=$APPPORT -jar $APPLOCATION >/dev/null 2>&1 &
+# echo "nohup java -Dspring.profiles.active=$APPENV -Dserver.port=$APPPORT -jar $APPLOCATION >/dev/null 2>&1 &"
+# nohup java -Dspring.profiles.active=$APPENV -Dserver.port=$APPPORT -jar $APPLOCATION >/dev/null 2>&1 &
+
+echo "nohup java -Dspring.config.location=$APPENV -Dserver.port=$APPPORT -jar $APPLOCATION >/dev/null 2>&1 &"
+nohup java -Dspring.config.location=$APPENV -Dserver.port=$APPPORT -jar $APPLOCATION >/dev/null 2>&1 &
 
 #
 tail -f $LOGLOCATION
